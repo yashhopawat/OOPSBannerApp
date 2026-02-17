607bda42c5d7ff564f3d863cb81099c2b2a4137a
yashhopawat <yo9198@srmist.edu.in>
2026-02-17 19:02:51 +0530

    joining strings

--- stringjoin.java
+++ stringjoin.java
@@ -6,20 +6,15 @@ public class stringjoin{
 
     public static void renderBanner() {
 
-        String line1 = String.join(" ", 
-                "*****",   "****",   "****",   "*****");
+        String line1 = String.join(" ", "*****",   "*****",  "****",  "*****");
 
-        String line2 = String.join(" ", 
-                "*   *",   "*   *",  "*   *",  "*");
+        String line2 = String.join(" ", "*   *",   "*   *",  "*  *",  "*    ");
 
-        String line3 = String.join(" ", 
-                "*   *",   "*   *",  "*   *",  "*");
+        String line3 = String.join(" ", "*   *",   "*   *",  "****",  "*****");
 
-        String line4 = String.join(" ", 
-                "*   *",   "*   *",  "*   *",  "*****");
+        String line4 = String.join(" ", "*   *",   "*   *",  "*   ",  "    *");
 
-        String line5 = String.join(" ", 
-                "*****",   "****",   "****",   "*****");
+        String line5 = String.join(" ", "*****",   "*****",  "*   ",  "*****");
 
         System.out.println(line1);
         System.out.println(line2);
