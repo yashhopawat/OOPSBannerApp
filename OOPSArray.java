b46dd252efe85a5ab07a4d0ee1dd0e30eb8a2078
yashhopawat <yo9198@srmist.edu.in>
2026-02-23 02:12:33 +0530

    UC$ - Implemented Banner using Array and Loops

---
+++ OOPSArray.java
@@ -0,0 +1,1 @@
+public class OOPSArray{ public static void main(String[] args) { String[] lines = { " *** *** ***** ***** ", "* * * * * * * ", "* * * * * * * ", "* * * * ***** ***** ", "* * * * * * ", "* * * * * * ", " *** *** * ***** " }; for (String line : lines) { System.out.println(line); } } }
