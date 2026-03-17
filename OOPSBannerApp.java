e1800da4f9c6c7917bc5b037457a5bd4f961617f
yashhopawat <yo9198@srmist.edu.in>
2026-03-17 08:47:42 +0530

    modified uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,12 @@
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****   ******   ****** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******   ****** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *        ****** ");
     }
 }
