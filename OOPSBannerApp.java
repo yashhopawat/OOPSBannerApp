a91bd66e0e4e9dfc825431ff08235be4a0520f38
yashhopawat <yo9198@srmist.edu.in>
2026-03-17 09:00:40 +0530

    modified uc3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,29 @@
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        renderBanner();
+    }
+
+    public static void renderBanner() {
+
+        String line1 = String.join(" ", 
+                "*****",   "*****",  "*****",   "*****");
+
+        String line2 = String.join(" ", 
+                "*   *",   "*   *",  "*   *",   "*");
+
+        String line3 = String.join(" ", 
+                "*   *",   "*   *",  "*****",   "*****");
+
+        String line4 = String.join(" ", 
+                "*   *",   "*   *",  "*    ",   "    *");
+
+        String line5 = String.join(" ", 
+                "*****",   "*****",  "*    ",   "*****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
